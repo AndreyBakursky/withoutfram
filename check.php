5bca5d1399a2752667ec415fd33954e0cd9ec81f
AndreyBakursky <68955115+AndreyBakursky@users.noreply.github.com>
2020-11-30 19:49:29 +0400

    Add files via upload

---
+++ check.php
@@ -0,0 +1,22 @@
+<?php
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $error='';
+    if(trim($email) == '')
+        $error='Введите ваш email';
+    else if(trim($message) == '')
+        $error='Введите вообщение';
+    else if(strlen($message) < 10)
+        $error = 'Сообщение должно быть длиннее';
+
+    if ($error != '') {
+        echo $error;
+        exit;
+    }
+
+    $subject = "=?utf-8?B?".base64_encode("Тестовое сообщение")."?=";
+    $headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html;charset=utf-8\r\n";
+
+    mail('a-bakur@mail.ru', $subject, $message, $headers);
+    header('Location: /about.php');
